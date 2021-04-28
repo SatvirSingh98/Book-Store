00ea393dc75c6a0f7b886878743c46047d158b56
satvir <satvirsingh.juc@gmail.com>
2021-04-28 19:15:24 +0530

    Part-2 Setting up basket app.

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('store.urls')),
+    path('bag/', include('basket.urls')),
     path('admin/', admin.site.urls),
 ]
 
